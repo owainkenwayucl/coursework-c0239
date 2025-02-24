07abf287526c02e5593227564e1d2d9f3e3bfaba
Owain Kenway <o.kenway@ucl.ac.uk>
2025-02-24 11:12:06 +0000

    SQAlchemy implementaiton

---
+++ data_management/lecture7a/sqa.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Table, Column, Integer, Float, String, MetaData, ForeignKey
+metadata = MetaData()
+molecules = Table('molecules', metadata,
+                  Column('name', String, primary_key=True),
+                  Column('mass', Float))
+
+atoms = Table('atoms', metadata,
+      Column('symbol', String, primary_key=True),
+      Column('number', Integer)
+             )
+
+atoms_in_molecules = Table('atoms_molecules', metadata,
+       Column('atom', None, ForeignKey('atoms.symbol')),
+       Column('molecule', None, ForeignKey('molecules.name')),
+       Column('number', Integer)
+)
+
+metadata.create_all(engine)
+print(metadata)

042153f52f3d0db18eef8c33bbd21dfd855669a2
Owain Kenway <o.kenway@ucl.ac.uk>
2025-02-27 10:54:42 +0000

    SQA postgresql

---
+++ data_management/lecture7b/postgresql_sqa_engine.py
@@ -0,0 +1,6 @@
+with open('/home/almalinux/.postgrespass', 'r') as passwordfile:
+    password = passwordfile.read().rstrip()
+server = ""
+username = "almalinux"
+database = "molecules_sqla"
+engine = sqlalchemy.create_engine(f'postgresql+psycopg2://{username}:{password}@{server}/{database}?sslmode=require')

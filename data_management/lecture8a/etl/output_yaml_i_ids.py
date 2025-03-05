5d050df27bacc756aa4558fa1b3b05353a8ff07e
Owain Kenway <o.kenway@ucl.ac.uk>
2025-03-05 12:20:35 +0000

    Do things properly

--- data_management/lecture8a/etl/output_yaml_i_ids.py
+++ data_management/lecture8a/etl/output_yaml_i_ids.py
@@ -1,53 +1,51 @@
 from chemistry_id import Molecule, Element, Reaction, System
 
 if __name__ == '__main__':
-    h = Element('H',1)
-    c = Element('C',16)
-    o = Element('O',12)
 
-    carbon_dioxide = Molecule(1)
+    system = System()
+
+    h = system.add_element('H')
+    c = system.add_element('C')
+    o = system.add_elementt('O')
+
+    carbon_dioxide = system.add_molecule()
     carbon_dioxide.add_element(c,1)
     carbon_dioxide.add_element(o,2)
 
-    oxygen = Molecule(2)
+    oxygen = system.add_molecule()
     oxygen.add_element(o,2)
 
-    hydrogen = Molecule(3)
+    hydrogen = system.add_molecule()
     hydrogen.add_element(h,2)
 
-    water = Molecule(4)
+    water = system.add_molecule()
     water.add_element(h,2)
     water.add_element(o,1)
 
-    glucose = Molecule(5)
+    glucose = system.add_molecule()
     glucose.add_element(c,6)
     glucose.add_element(h,12)
     glucose.add_element(o,6)
 
-    methane = Molecule(5)
+    methane = system.add_molecule()
     methane.add_element(c,1)
     methane.add_element(h,4)
 
-    burning_glucose = Reaction()
+    burning_glucose = system.add_reaction()
     burning_glucose.add_reactant(glucose,1)
     burning_glucose.add_reactant(oxygen,6)
     burning_glucose.add_product(carbon_dioxide, 6)
     burning_glucose.add_product(water, 6)
 
-    burning_hydrogen = Reaction()
+    burning_hydrogen = system.add_reaction()
     burning_glucose.add_reactant(hydrogen,2)
     burning_glucose.add_reactant(oxygen,1)    
     burning_glucose.add_product(water, 2)
 
-    burning_methane = Reaction()
+    burning_methane = system.add_reaction()
     burning_methane.add_reactant(methane,1)
     burning_methane.add_reactant(oxygen, 2)
     burning_methane.add_product(carbon_dioxide,1)
     burning_methane.add_product(water,2)
 
-    reactions = System()
-    reactions.add_reaction(burning_glucose)
-    reactions.add_reaction(burning_hydrogen)
-    reactions.add_reaction(burning_methane)
-
-    print(yaml.dump(reactions.save()))
+    print(yaml.dump(system.save()))

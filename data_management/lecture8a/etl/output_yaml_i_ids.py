19847f5456bf98d4775a7a03976f75729ad0d5fd
Owain Kenway <o.kenway@ucl.ac.uk>
2025-03-05 13:15:46 +0000

    Switch back to yaml

--- data_management/lecture8a/etl/output_yaml_i_ids.py
+++ data_management/lecture8a/etl/output_yaml_i_ids.py
@@ -1,5 +1,5 @@
 from chemistry_id import System
-import json
+import yaml
 
 if __name__ == '__main__':
 
@@ -49,4 +49,4 @@ if __name__ == '__main__':
     burning_methane.add_product(carbon_dioxide,1)
     burning_methane.add_product(water,2)
 
-    print(json.dumps(system.save()))
+    print(yaml.dump(system.save()))

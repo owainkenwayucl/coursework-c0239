91d01510e0db8326d23634d9551e508dba805055
Owain Kenway <o.kenway@ucl.ac.uk>
2025-03-06 11:02:59 +0000

    many fixes

--- ci-cd/runners/install.sh
+++ ci-cd/runners/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -eo
+
 # Create a folder
 mkdir actions-runner && cd actions-runner
 # Download the latest runner package
 curl -o actions-runner-linux-x64-2.322.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.322.0/actions-runner-linux-x64-2.322.0.tar.gz
 # Optional: Validate the hash
-echo "b13b784808359f31bc79b08a191f5f83757852957dd8fe3dbfcc38202ccf5768  actions-runner-linux-x64-2.322.0.tar.gz" | shasum -a 256 -c
+echo "b13b784808359f31bc79b08a191f5f83757852957dd8fe3dbfcc38202ccf5768  actions-runner-linux-x64-2.322.0.tar.gz" | sha256sum -c
 # Extract the installer
 tar xzf ./actions-runner-linux-x64-2.322.0.tar.gz

731034a7c75b728eaa652ecb286b4ad39e0f28d4
Owain Kenway <o.kenway@ucl.ac.uk>
2025-03-10 16:26:29 +0000

    COnnect to localhost
    Disable CA certificate bundle as we are connecting to the ingress endpoint

--- data_management/lecture9a/full_model/test/fixtures/trino.py
+++ data_management/lecture9a/full_model/test/fixtures/trino.py
@@ -1,11 +1,10 @@
-from ...discover import remote_host
 from trino.dbapi import connect
 import contextlib
 
 @contextlib.contextmanager
 def remote_trino(stage):
     conn = connect(
-        host=remote_host(stage, "Trino"),
+        host="localhost",
         port=80,
         user="almalinux",
         catalog="hive"

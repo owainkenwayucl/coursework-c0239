96dc5c63a1dd951cb98a8ee790d264de856c9194
Owain Kenway <o.kenway@ucl.ac.uk>
2025-03-12 17:04:55 +0000

    XPATH with namespaces

---
+++ data_management/lecture9b/XML/xpath_uri.py
@@ -0,0 +1,7 @@
+from lxml import etree
+tree = etree.parse(open('system.xml'))
+print("Without namespace: ")
+print(tree.xpath('//molecule/element[@number="1"]/@symbol'))
+print("With namepace: ")
+namespaces={'r': 'http://arc.ucl.ac.uk/schema/reaction/'}
+tree.xpath('//r:molecule/r:element[@number="1"]/@symbol', namespaces = namespaces)

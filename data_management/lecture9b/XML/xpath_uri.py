7a3f10b7444dbb5caf4f17e2e9711bd769162847
Owain Kenway <o.kenway@ucl.ac.uk>
2025-03-12 17:05:48 +0000

    It helps if we use the right file

--- data_management/lecture9b/XML/xpath_uri.py
+++ data_management/lecture9b/XML/xpath_uri.py
@@ -1,5 +1,5 @@
 from lxml import etree
-tree = etree.parse(open('system.xml'))
+tree = etree.parse(open('uriexample.xml'))
 print("Without namespace: ")
 print(tree.xpath('//molecule/element[@number="1"]/@symbol'))
 print("With namepace: ")

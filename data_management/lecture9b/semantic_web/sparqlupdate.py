cd363bd8bfd29c23fa079dfb8b6dea712bfd952f
Owain Kenway <o.kenway@ucl.ac.uk>
2025-03-14 12:05:46 +0000

    Do updates

---
+++ data_management/lecture9b/semantic_web/sparqlupdate.py
@@ -0,0 +1,35 @@
+from jinja2 import Template
+from full_model.test.fixtures.tiny_db import tiny_db
+from rdflib import Graph
+
+mytemplate = Template(open('system.ttl.j2').read())
+with open('system.ttl','w') as ttlfile:
+    ttlfile.write((mytemplate.render(reactions=tiny_db())))
+
+graph = Graph()
+graph.parse("system.ttl", format="ttl")
+
+results=graph.query(
+    """SELECT DISTINCT ?asymbol ?bsymbol
+       WHERE {
+          ?molecule arcr:hasElementQuantity ?a .
+          ?a arcr:countedElement ?elementa .
+          ?elementa arcr:symbol ?asymbol .
+          ?molecule arcr:hasElementQuantity ?b .
+          ?b arcr:countedElement ?elementb .
+          ?elementb arcr:symbol ?bsymbol
+       }""")
+
+q = """
+SELECT DISTINCT ?asymbol ?bsymbol
+WHERE {
+        ?moleculea arcr:inMoleculeWith ?moleculeb .
+        ?elementa arcr:symbol ?asymbol .
+        ?elementb arcr:symbol ?bsymbol
+    }
+"""
+
+results = graph.query(q)
+
+for row in results:
+    print("Elements %s and %s are found in the same molecule" % row)
